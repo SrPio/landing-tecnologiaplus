094f8ce2c831cd6e1772ee882e44079291ab95fb
SrPio <andresfer1799@gmail.com>
2025-02-06 19:16:50 -0500

    Replaces LogosCarousel with InfiniteSlider
    Updates the clients section to use InfiniteSlider instead of LogosCarousel for displaying client logos. Changes the source of logos from logosTurnos to logosTermicos.

--- src/turnero-turnoexpress/components/ClientsSection/TurnoExpressClientsSection.jsx
+++ src/turnero-turnoexpress/components/ClientsSection/TurnoExpressClientsSection.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import style from "./TurnoExpressClientsSection.module.scss";
 import { useLogos } from "../../../context/LogosContext";
 import LogosCarousel from "../../../components/sliders/LogosCarousel";
+import InfiniteSlider from "../../../components/sliders/InfiniteSlider";
 
 function TurnoExpressClientsSection() {
-  const { logosTurnos } = useLogos();
+  const { logosTermicos } = useLogos();
 
   return (
     <section id="clients-section" className={style.clients__section}>
       <h2 className={style.clients__title}>Algunos de nuestros clientes</h2>
       <p>"Eliminan las filas con nuestro Turnero Digital."</p>
       <div className={style.clients__container}>
-        <LogosCarousel logos={logosTurnos} />
+        <InfiniteSlider logos={logosTermicos} />
       </div>
     </section>
   );

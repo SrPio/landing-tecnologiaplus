a7b5a3454baee37ec841f5275de5958df600615d
SrPio <andresfer1799@gmail.com>
2025-02-08 10:12:43 -0500

    Refactors HeroSection layout and styles
    Encapsulates hero image and text within a central container
    Adjusts positioning and dimensions for better responsiveness
    Cleans up and comments out unused CSS rules

--- src/rollos-turnos/components/HeroSection/TurnosHeroSection.jsx
+++ src/rollos-turnos/components/HeroSection/TurnosHeroSection.jsx
@@ -6,23 +6,24 @@ function TurnosHeroSection() {
     <section id="hero-section" className={styles.hero}>
       <Header />
       <div className={styles.hero__content}>
-        <img
-          loading="lazy"
-          className={styles.hero__img}
-          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349967/Imagen_banner_2x-8_kcq3mt_v22bb7.webp"
-          alt="imagen de rollos para impresora"
-        />
-
-        <div className={styles.hero__text}>
-          <h1>
-            Sistemas de llamado
-            <br />
-            inalámbrico para mejorar
-          </h1>
-          <p>la atención al cliente</p>
-          <a className={styles.hero__btn} href="#">
-            Ver más
-          </a>
+        <div className={styles.central__container}>
+          <img
+            loading="lazy"
+            className={styles.hero__img}
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349967/Imagen_banner_2x-8_kcq3mt_v22bb7.webp"
+            alt="imagen de rollos para impresora"
+          />
+          <div className={styles.hero__text}>
+            <h1>
+              Sistemas de llamado
+              <br />
+              inalámbrico para mejorar
+            </h1>
+            <p>la atención al cliente</p>
+            <a className={styles.hero__btn} href="#">
+              Ver más
+            </a>
+          </div>
         </div>
       </div>
     </section>

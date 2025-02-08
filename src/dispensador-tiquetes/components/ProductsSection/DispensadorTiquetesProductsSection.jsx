5b054eaa36ed19875e76229348f16b7edbc8e95f
SrPio <andresfer1799@gmail.com>
2025-02-08 10:17:12 -0500

    Adds product description and improves text wrapping
    Inserts a paragraph describing the benefits of the ticket dispenser
    Enhances text readability by balancing text wrapping in the product section

--- src/dispensador-tiquetes/components/ProductsSection/DispensadorTiquetesProductsSection.jsx
+++ src/dispensador-tiquetes/components/ProductsSection/DispensadorTiquetesProductsSection.jsx
@@ -13,6 +13,13 @@ function DispensadorTiquetesProductsSection() {
         <a className={style.card__btn} href="#">
           Ver más
         </a>
+
+        <p>
+          Elimina las filas y agiliza el flujo de atención, nuestro dispensador
+          de turnos ayuda a reducir el desorden y mejora la experiencia de
+          espera, asegurando que cada cliente sea atendido en el momento
+          adecuado y sin confusiones.
+        </p>
       </div>
     </section>
   );
